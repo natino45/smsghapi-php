78fa34230e8828ff18257b5a6a96b66904a7e1ef
Tochemey <tochemey@gmail.com>
2013-12-09 13:08:10 +0000

    Fix some typos in the code.

--- demo.php
+++ demo.php
@@ -2,35 +2,38 @@
 
 include 'Smsgh/Api.php';
 
-$apiHost = new Smsgh_ApiHost;
-$apiHost->clientId('user123');
-$apiHost->clientSecret('secret');
+$apiHost = new SmsghApi();
+$apiHost->setClientId('user123');
+$apiHost->setClientSecret('secret');
+$apiHost->setContextPath("v3");
+$apiHost->setHttps(true);
+$apiHost->setHostname("api.smsgh.com");
 
 try {
 	/**
 	 * Sending a simple message.
 	 */
-	$apiHost->messagesResource()
+	$apiHost->getMessages()
 		->send('SMSGH', '+233248183783', 'Hello world!');
 		
 	/**
 	 * Sending a message with extended properties.
 	 */
-	$apiMessage = new Smsgh_ApiMessage;
-	$apiMessage->from('SMSGH');
-	$apiMessage->to('+233248183783');
-	$apiMessage->content('Hello world!');
-	$apiMessage->registeredDelivery(true);
-	$apiHost->messagesResource()->send($apiMessage);
+	$apiMessage = new ApiMessage();
+	$apiMessage->setFrom('SMSGH');
+	$apiMessage->setTo('+233248183783');
+	$apiMessage->setContent('Hello world!');
+	$apiMessage->setRegisteredDelivery(true);
+	$apiHost->getMessages()->send($apiMessage);
 	
 	/**
 	 * Scheduling a message.
 	 */
-	$apiMessage = new Smsgh_ApiMessage;
-	$apiMessage->from('SMSGH');
-	$apiMessage->to('+233248183783');
-	$apiMessage->content('Hello, world!');
-	$apiHost->messagesResource()->schedule($apiMessage, 'tomorrow');
+	$apiMessage = new ApiMessage();
+	$apiMessage->setFrom('SMSGH');
+	$apiMessage->setTo('+233248183783');
+	$apiMessage->setContent('Hello, world!');
+	$apiHost->getMessages()->schedule($apiMessage, 'tomorrow');
 } catch (Smsgh_ApiException $ex) {
 	echo 'ERROR: ', $ex->message(), "\n";
 }
